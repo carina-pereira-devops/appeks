57dc52ea202aa00e5cdf1dc676dfbb5c49224039
carina-pereira-devops <devops79@yahoo.com>
2025-06-28 17:04:20 -0300

    Inicializando variáveis

--- iac_eks/variables.tf
+++ iac_eks/variables.tf
@@ -2,21 +2,27 @@
 variable "cidr_block" {
   type        = string
   description = "Range de IPs utilizado na VPC"
+  default = "10.0.0.0/16"
 }
 
 variable "project_name" {
   type        = string
   description = "Nome do Projeto"
+  default = "ekspp"
 }
 
 variable "region" {
   type        = string
   description = "Região da AWS"
+  default = "us-east-1"
 }
 
 variable "tags" {
   # Variável com conjunto de pares chave-valor 
   type        = map(any)
   description = "Tags pertencentes aos recursos criados"
+  default = {
+    "Time": "SRE"
+  }
 }
 
