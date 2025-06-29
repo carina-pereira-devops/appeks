dfe88e06311da1f3b71932a8af2ee5347dec7370
carina-pereira-devops <devops79@yahoo.com>
2025-06-29 11:31:02 -0300

    Ajuste no Helm Prometheus

--- iac_eks/modules/prometheus/main.tf
+++ iac_eks/modules/prometheus/main.tf
@@ -3,12 +3,13 @@ resource "helm_release" "prometheus" {
  repository = "https://prometheus-community.github.io/helm-charts"
  chart      = "prometheus"
  namespace = "app"
- #create_namespace = true
-
+ 
  set {
    name  = "server.service.type"
    value = "LoadBalancer"
  }
+  wait = true
+  dependency_update = true
 
  set {
    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
