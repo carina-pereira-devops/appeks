31109a89113fbf549aa769afd5baa4fe842f5e57
carina-pereira-devops <devops79@yahoo.com>
2025-07-14 17:21:42 -0300

    Destruindo testes

--- iac_eks/modules/prometheus/main.tf
+++ iac_eks/modules/prometheus/main.tf
@@ -3,7 +3,6 @@ resource "helm_release" "prometheus" {
  repository = "https://prometheus-community.github.io/helm-charts"
  chart      = "prometheus"
  namespace  = "prometheus"
-
  create_namespace = true
 
 # set {
