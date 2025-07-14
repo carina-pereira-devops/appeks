7dbcb9f3a0cb48139e34d90ece75fa427f21cf6e
carina-pereira-devops <devops79@yahoo.com>
2025-07-14 20:50:48 -0300

    Add ALB Traefick

--- iac_eks/modules/estrutura/traefik.tf
+++ iac_eks/modules/estrutura/traefik.tf
@@ -4,4 +4,24 @@ resource "helm_release" "traefik" {
   repository       = "https://traefik.github.io/charts"
   chart            = "traefik"
   create_namespace = true
+
+ set {
+   name  = "server.service.type"
+   value = "LoadBalancer"
+ }
+
+ set {
+   name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+   value = "alb"
+ }
+
 }
+
+ data "kubernetes_service" "traefik_server" {
+ metadata {
+   name      = "traefik-server"
+   namespace = helm_release.traefik.namespace
+ }
+ }
+
+
