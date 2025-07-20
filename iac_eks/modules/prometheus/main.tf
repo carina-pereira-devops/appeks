d70a697417eba9d0d99f1750a2b7ca91e90440f8
carina-pereira-devops <devops79@yahoo.com>
2025-07-20 20:47:14 -0300

    Ajuste na configuração do k8s_Prometheus

--- iac_eks/modules/prometheus/main.tf
+++ iac_eks/modules/prometheus/main.tf
@@ -31,3 +31,34 @@ resource "kubernetes_namespace" "prometheus-namespace" {
     name = "observability"
   }
 }
+
+
+module "prometheus_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                                        = "role_prometheus"
+  attach_amazon_managed_service_prometheus_policy  = true
+  amazon_managed_service_prometheus_workspace_arns = [module.prometheus.workspace_arn]
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc
+      namespace_service_accounts = ["${kubernetes_namespace.prometheus-namespace.metadata[0].name}:prometheus"]
+    }
+  }
+
+}
+
+resource "kubernetes_service_account" "service-account" {
+  metadata {
+    name      = "prometheus"
+    namespace = kubernetes_namespace.prometheus-namespace.metadata[0].name
+    labels = {
+      "app.kubernetes.io/name" = "prometheus"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn"               = module.prometheus_role.iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
---
+++ iac_eks/modules/prometheus/variables.tf
@@ -0,0 +1,4 @@
+variable "oidc" {
+  type        = string
+  description = "Autenticação OIDC do Cluster"
+}
